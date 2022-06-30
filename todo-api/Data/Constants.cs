1759bb6de7ba5d82171765a5ae34baad0d7393b1
jonasss312 <jonasss312@gmail.com>
2022-06-30 22:54:13 +0300

    [REFACTOR]moved-text-to-constants-file

---
+++ todo-api/Data/Constants.cs
@@ -0,0 +1,19 @@
+﻿namespace todo_api.Data
+{
+    public static class Constants
+    {
+        public const string ERROR_INVALID_LOGIN_DETAILS = "Username or password invalid";
+        public const string ERROR_CANNOT_CREATE_NEW_USER = "Username or password invalid";
+        public const string ERROR_ASSIGNMENT_NOT_FOUND = "Assignment not found.";
+        public const string ERROR_ASSIGNMENT_VALIDATION = "Assignment name and status required";
+        public const string INVALID_LOGIN_DETAILS = "Username or password invalid";
+
+        public const string API_PATH = "api/";
+        public const string USERS_PATH = "users/";
+        public const string ASSIGNMENTS_PATH = "assignments/";
+
+        public const string USER_ID = "{userId}";
+        public const string ASSIGNMENT_ID = "{assignmentId}";
+
+    }
+}
