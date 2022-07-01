1c3257de5d991ff0bc8a1f7608d54c27e6d34be8
jonasss312 <jonasss312@gmail.com>
2022-07-01 08:38:04 +0300

    [FIX]merged-conflict

--- todo-api/Controllers/UserController.cs
+++ todo-api/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using System.ComponentModel.DataAnnotations;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
@@ -9,9 +10,9 @@ using todo_api.Models.Dtos;
 
 namespace todo_api.Controllers
 {
-    [Route("api/users/")]
+    [Route(Constants.API_PATH + Constants.USERS_PATH)]
     [ApiController]
-    public class UserController:ControllerBase
+    public class UserController : ControllerBase
     {
         private UserRepo _userRepo;
         private IConfiguration _configuration;
@@ -27,8 +28,11 @@ namespace todo_api.Controllers
         public async Task<ActionResult<User>> Register(UserDto registerUserDto)
         {
             var user = await _userRepo.GetUserByEmail(registerUserDto.Email);
+
             if(user != null)
-                return BadRequest("User already exists");
+                return BadRequest(Constants.ERROR_CANNOT_CREATE_NEW_USER);
+            if(!new EmailAddressAttribute().IsValid(registerUserDto.Email))
+                return BadRequest(Constants.ERROR_INVALID_EMAIL);
 
             User newUser = CreateNewUser(registerUserDto);
             await _userRepo.Insert(newUser);
@@ -61,11 +65,12 @@ namespace todo_api.Controllers
         public async Task<ActionResult<string>> Login(UserDto loginDto)
         {
             var user = await _userRepo.GetUserByEmail(loginDto.Email);
+
             if(user == null)
-                return BadRequest("Username or password invalid");
+                return BadRequest(Constants.ERROR_INVALID_LOGIN_DETAILS);
 
             if(!VerifyPasswordHash(loginDto.Password, user.PasswordHash, user.PasswordSalt))
-                return BadRequest("Username or password invalid");
+                return BadRequest(Constants.ERROR_INVALID_LOGIN_DETAILS);
 
             return Ok(CreateToken(user));
         }
